9210df68129282c8524aa65279807ebcd8eabde3
tokyoyoshida <yoshidaforpublic@gmail.com>
2020-07-19 05:39:27 +0900

    add files

---
+++ CiderDetector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CiderDetector
+//
+//  Created by TokyoMac on 2020/07/19.
+//  Copyright © 2020 TokyoMac. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
